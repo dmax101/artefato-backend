760f3fcfde3ffea4b936b13708ff83ba10bf971d
Danilo Ribeiro <danriba@gmail.com>
2021-10-27 17:07:17 -0300

    Minor Adjustments

--- src/main/java/br/inatel/icc/idp/artefato/controller/ProductController.java
+++ src/main/java/br/inatel/icc/idp/artefato/controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.util.Pair;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/product")
+@CrossOrigin(origins = "http://localhost:4200")
 @Validated
 @Slf4j
 public class ProductController {
